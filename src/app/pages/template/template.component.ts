2646254b5830288603a62648674d98bcf3c09a41
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-18 13:40:36 +0100

    Default Pais Opt@Select through TSobject

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -12,7 +12,8 @@ export class TemplateComponent implements OnInit {
   usuario={
     nombre: 'Usuario',
     apellido: 'Asdds',
-    correo: 'asdf@sdf.fd'
+    correo: 'asdf@sdf.fd',
+    pais:'ESP'
   }
 
   paises:any[]=[];
@@ -22,15 +23,20 @@ export class TemplateComponent implements OnInit {
     this.paisSvc.getPaises().subscribe(paises=>{
       console.log(paises);
       this.paises=paises;
+      this.paises.unshift({
+        nombre: '[Seleccione país]',
+        codigo: ''
+      })
     });
   }
 
   guardar(forma:NgForm){
     if (forma.invalid){
       Object.values(forma.controls).forEach(control=>{
-        // console.log(control);
+        console.log(control);
         control.markAsTouched();
       });
     }
+    console.log(forma);
   }
 }
