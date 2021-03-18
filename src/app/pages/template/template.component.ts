349fbb100724291c6d41d120c48c00d91505b037
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-18 12:23:12 +0100

    ngModel working

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  usuario={
+    nombre: 'default'
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  guardar(forma:NgForm){
+    console.log(this.usuario);
+  }
 }
