7e58bd004921940a327d905db3ed7040c989219b
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-19 08:39:53 +0100

    Validación TS FormBuilder, FormGroup, Validators

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -17,9 +17,9 @@ export class ReactiveComponent implements OnInit {
   }
   crearForm(){
     this.forma=this.formBuilder.group({
-      nombre: ['defaultValue'],
-      apellido: [''],
-      correo: ['']
+      nombre: ['',[Validators.required, Validators.minLength(5)]], //1ºarg: defaultValue. 2ºarg: validadores sincronos (no requieren acceso exterior), 3º validadores asíncronos
+      apellido: ['',Validators.required],
+      correo: ['',[Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z0]{2,3}$'), Validators.required]]
     });
   }
   guardar(){
